e0d9ca4bf7762c20924eb19af0587dca6085ebb2
mancj <mancjone@gmail.com>
2024-06-01 21:41:16 +0300

    lesson 2

--- bin/lesson_2.dart
+++ bin/lesson_2.dart
@@ -1,4 +1,11 @@
 void main(List<String> args) {
+// Типы переменных
+  int a = 200; // целое число
+  double d = 200.5; // дробное число (значение с плавающей точкой)
+  bool b = true; // булево значение (истина/ложь)
+  String name = "Mansur"; // строка, значение обязательно дожлно быть в кавычках
+
+  // Environment variables
   bool isAdmin = false;
 
   bool isPremium = true;
@@ -17,7 +24,13 @@ void main(List<String> args) {
   print("Program exit");
 
   /*
-    if (isAdmin) {
+   if (если) - пишется в начале, начало условной конструкции
+    else if (иначе если)  - если предыдущее условие не соблюдено, проверяется данное
+    else (иначе) - выполнится если не одно из условий не выполнено
+    */
+
+  /*
+   if (isAdmin) {
       int rating = 100;
       print("The user is admin");
       print("Rating");
@@ -30,4 +43,15 @@ void main(List<String> args) {
       print("The user normal");
     }
   */
+
+  int rating = 100;
+  bool isAdmin2;
+
+  if (rating > 50) {
+    isAdmin2 = true;
+  } else {
+    isAdmin2 = false;
+  }
+
+  print("Is mansur: $isAdmin2");
 }
